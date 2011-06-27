b440539919622d70cc1ae8700fc6061a21a134ad
airways <isaac.raway@gmail.com>
2011-06-27 15:49:34 +0000

    ProLib: Add generic a.pl_confirm class to prolib javascript to display rel text as a confirm() prompt before visiting the link
    Mason:
    Added field command system to edit_block_type
    Added remove field command
    Added field additional column name handling 
    
    
    git-svn-id: https://svn.metasushi.net/ee/trunk/addons/prolib@255 59723c01-6204-4710-ac68-99f48fd4fa17

---
+++ themes/javascript/prolib.js
@@ -0,0 +1,16 @@
+var prolib = {
+
+    bind_events: function() {
+        $('a.pl_confirm').click(function(event) {
+            if(!confirm($(this).attr('rel'))) {
+                return false;
+            }
+        })
+    }
+
+}
+
+
+$(document).ready(function() {
+    prolib.bind_events();
+});
