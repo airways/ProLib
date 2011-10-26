f184c2b750381d579c63aef62563f8a1fef88ca1
Isaac Raway <isaac.raway@gmail.com>
2011-10-26 11:50:58 -0500

    Fix bug in calling replace_tag - was checking for wrong method on instance object

--- libraries/bm_celltypes.php
+++ libraries/bm_celltypes.php
@@ -406,7 +406,7 @@ class BM_CellType {
      */
     function replaceType($replaceMethod, &$entry_row, $field_id, $row_id, $col_id, $data, $params = array(), $tagdata = FALSE)
     {
-        if (method_exists($this->instance, 'save_cell_settings'))
+        if (method_exists($this->instance, 'replace_tag'))
         {
             $this->instance->row = &$entry_row;
             $this->instance->field_id = $field_id;
