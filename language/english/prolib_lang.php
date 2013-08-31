e6f18d06b04ce7783e2865a0a6548ab5817a5402
Nathanael Raway <nathanael.raway@gmail.com>
2013-08-31 21:44:45 +0000

    Fixed nonfunctioning validation types 'matches_value' and 'numberic_dashes' [667]

--- language/english/prolib_lang.php
+++ language/english/prolib_lang.php
@@ -5,7 +5,8 @@ require_once PATH_THIRD.'prolib/config.php';
 $lang = array(
     'prolib_module_name' => PROLIB_NAME,
     'prolib_module_description' => PROLIB_DESCRIPTION,
-    
+    'matches_value' => 'The %s field must be the value "%s"',
+    'numeric_dash' => 'The %s field must contain numbers and dashes only',
     
     // END
     '' => '',
