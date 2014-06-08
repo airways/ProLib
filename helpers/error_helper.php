eaa154d35c8dbd013956a9c7360f95633dab670f
Isaac Raway <isaac@mm.st>
2014-06-08 14:46:21 -0500

    Fix pl_show_error, switched to prolib templates directory for error template, fixed base driver's magic __call method to return the last value of params array if the hook is undefined (as already expected)

--- helpers/error_helper.php
+++ helpers/error_helper.php
@@ -1,26 +1,24 @@
 <?php
 
 
-function pl_show_error($message, $heading = 'Error', $homepage = '')
+function pl_show_error($message, $heading = 'Error', $homepage = '', $object = false)
 {
-
-    $message .= '<div class="back_link"><a href="javascript:history.go(-1);">Back</a></div>';
-
     $EE = &get_instance();
     if($EE->config->item('prolib_error_template'))
     {
         $file = $EE->config->item('prolib_error_template');
     } else {
-        $file = APPPATH.'views/errors/error_general.php';
+        $file = realpath(join(DIRECTORY_SEPARATOR, array(__DIR__, '..', 'templates', 'error.php')));
     }
-    
+
     if(file_exists($file) && file_get_contents($file))
     {
         include($file);
     } else {
         echo '<h2>'.$heading.$homepage.'</h2>';
         echo $message;
+        echo '<div class="back_link"><a href="javascript:history.go(-1);">Back</a></div>';
     }
-    
+
     exit;
 }
