834b9824fd0cd67773b736b6d8c30d38510780e2
Isaac Raway <isaac@mm.st>
2014-09-15 21:15:44 -0500

    Bump version to 0.77, added pl_form_hidden to provide hidden element with ID

--- libraries/pl_forms.php
+++ libraries/pl_forms.php
@@ -370,3 +370,22 @@ class PL_forms {
     } // function _render_grid
 
 }
+
+
+function pl_form_hidden($name, $value = '', $id = false)
+{
+    if ( ! is_array($name))
+    {
+        return '<input type="hidden" id="'.($id ? $id : $name).'" name="'.$name.'" value="'.form_prep($value).'" />';
+    }
+
+    $form = '';
+
+    foreach ($name as $name => $value)
+    {
+        $form .= "\n";
+        $form .= '<input type="hidden"  id="'.($id ? $id : $name).'" name="'.$name.'" value="'.form_prep($value).'" />';
+    }
+
+    return $form;
+}
