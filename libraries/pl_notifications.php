8f4d91ef306a4bc7e874db6ba29273dc8d8a67b6
Isaac Raway <isaac@mm.st>
2015-03-09 12:42:39 -0700

    Improve notification debug formatting a bit

--- libraries/pl_notifications.php
+++ libraries/pl_notifications.php
@@ -35,7 +35,7 @@ class PL_Notifications {
     protected function _debug($msg, $escape=TRUE)
     {
 
-        $this->debug_str .= ($escape ? htmlentities($msg) : $msg ) . '<br/>';
+        $this->debug_str .= ' - ' . date('%D %H:%i:%s.$u') . ' ' . (($escape ? htmlentities($msg) : $msg )) . '<hr/>';
     }
 
     public function clear_attachments()
