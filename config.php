aec9c2c4df8feccd369254284234c5800ca01945
Isaac Raway <isaac@mm.st>
2015-11-12 09:02:44 -0600

    Bump version to 0.93

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-//@version 0.92
+//@version 0.93
 
-define('PROLIB_VERSION', '0.92');
+define('PROLIB_VERSION', '0.93');
 define('PROLIB_NAME', 'Prolib');
 define('PROLIB_CLASS', 'Prolib'); // must match module class name
 define('PROLIB_DESCRIPTION', 'Prolib is a collection of libraries and helpers to make development of ExpressionEngine add-ons easier.');
